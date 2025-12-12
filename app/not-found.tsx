cd44b68bb85396f01f39727d8d19335d8b074534
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2025-12-12 23:07:22 +0100

    export metadata for the 404 page.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,22 +1,28 @@
-'use client';
 import css from './page.module.css';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 
-const NotFound = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => router.push('/'), 2000);
-    return () => clearTimeout(timer);
-  }, [router]);
-
-  return (
-    <>
-      <h1 className={css.title}>404 - Page not found</h1>
-      <p className={css.description}>Sorry, the page you are looking for does not exist.</p>
-    </>
-  );
+export const metadata = {
+  title: '404 — Page not found',
+  description: 'The requested page was not found on NoteHub.',
+  openGraph: {
+    title: '404 — Page not found',
+    description: 'The requested page was not found on NoteHub.',
+    // url: 'https://my-deployed-site.vercel.app/not-found',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub App',
+      },
+    ],
+  },
 };
 
-export default NotFound;
+export default function NotFound() {
+  return (
+    <>
+      <h1>404 — Page not found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </>
+  );
+}
