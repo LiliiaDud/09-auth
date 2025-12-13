e34bb0475ffc5e00d1313380789cb3b919b1995f
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2025-12-13 19:36:12 +0100

    implement generateMetadata for the filter page

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,11 +1,33 @@
 import { fetchNotes } from '@/lib/api';
 import NotesClient from './Notes.client';
-
+import type { Metadata } from 'next';
 import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
 
 type Props = {
   params: Promise<{ slug: string[] }>;
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const tag = slug?.[0];
+  return {
+    title: `Notes tagged with:${tag}`,
+    description: `${tag}`,
+    openGraph: {
+      title: `Notes tagged with:${tag}`,
+      description: `${tag}`,
+      // url: `https://my-deployed-site.vercel.app/notes/filter/${tag}`,
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'NoteHub App',
+        },
+      ],
+    },
+  };
+}
 
 export default async function Notes({ params }: Props) {
   const queryClient = new QueryClient();
