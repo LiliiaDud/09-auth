ff9404ba24f72a2168ee0f7da2ea3a3e95087b44
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2025-12-18 16:01:49 +0100

    change NotesClient

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -2,63 +2,61 @@
 
 import css from './NotesPage.module.css';
 import { useState } from 'react';
-import { useDebounce } from 'use-debounce';
+import { useDebouncedCallback } from 'use-debounce';
 import { useQuery } from '@tanstack/react-query';
 import { fetchNotes } from '@/lib/api';
+import { Note } from '@/types/note';
 import { keepPreviousData } from '@tanstack/react-query';
-import { Toaster } from 'react-hot-toast';
 import SearchBox from '@/components/SearchBox/SearchBox';
 import Pagination from '@/components/Pagination/Pagination';
 import NoteList from '@/components/NoteList/NoteList';
-import Modal from '@/components/Modal/Modal';
-import NoteForm from '@/components/NoteForm/NoteForm';
+import { useRouter } from 'next/navigation';
 
 interface Props {
   tag?: string;
 }
 
 export default function NotesClient({ tag }: Props) {
+  const router = useRouter();
   const [page, setPage] = useState(1);
   const [perPage] = useState(12);
   const [search, setSearch] = useState('');
-  const [debouncedSearch] = useDebounce(search, 400);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { data } = useQuery({
-    queryKey: ['notes', page, debouncedSearch, tag],
-    queryFn: () => fetchNotes({ page, perPage, search: debouncedSearch, tag }),
+    queryKey: ['notes', { page, perPage, search, tag }],
+    queryFn: () => fetchNotes({ page, perPage, search, tag }),
     staleTime: 60 * 1000,
     placeholderData: keepPreviousData,
-    refetchOnMount: false,
+    // refetchOnMount: false,
   });
 
-  const handleSearch = (newValue: string) => {
+  const notes = (data?.notes as Note[]) || [];
+  const totalPages = data?.totalPages || 0;
+
+  const handleSearch = useDebouncedCallback((value: string) => {
     setPage(1);
-    setSearch(newValue);
+    setSearch(value);
+  }, 400);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    router.push('/notes/action/create');
   };
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox onChange={handleSearch} value={search} />
-
-        {data && data.totalPages > 1 && (
-          <Pagination pageCount={data.totalPages} currentPage={page} onPageChange={setPage} />
+        <SearchBox onChange={handleSearch} />
+        {totalPages > 1 && (
+          <Pagination currentPage={page} pageCount={totalPages} onPageChange={setPage} />
         )}
 
-        <button className={css.button} onClick={() => setIsModalOpen(true)}>
+        <button className={css.button} onClick={handleClick}>
           Create note +
         </button>
       </header>
 
-      {data && <NoteList notes={data.notes} />}
-
-      {isModalOpen && (
-        <Modal onClose={() => setIsModalOpen(false)}>
-          <NoteForm onCancel={() => setIsModalOpen(false)} />
-        </Modal>
-      )}
-      <Toaster />
+      {notes.length > 0 && <NoteList notes={notes} />}
     </div>
   );
 }
