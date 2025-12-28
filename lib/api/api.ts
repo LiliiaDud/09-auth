994eeb63ec75aeb1b1b5937cd9a101fcf05e176e
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2025-12-28 23:58:48 +0100

    Wild idea to use other vercel system env

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const vercelApiUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : undefined;
+const vercelApiUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : undefined;
 const fallbackApiUrl = 'http://localhost:3000';
 const ApiBaseUrl = process.env.NEXT_PUBLIC_API_URL ?? vercelApiUrl ?? fallbackApiUrl;
 
