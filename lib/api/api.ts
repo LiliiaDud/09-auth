fa0c40075a69a7f1318557b5630fc18195ee424f
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2025-12-28 23:41:02 +0100

    Remove deploy config logic from code

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-const vercelApiUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : undefined;
-const fallbackApiUrl = 'http://localhost:3000';
-const ApiBaseUrl = process.env.NEXT_PUBLIC_API_URL ?? vercelApiUrl ?? fallbackApiUrl;
-
 export const client = axios.create({
-  baseURL: ApiBaseUrl + '/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
   withCredentials: true,
 });
