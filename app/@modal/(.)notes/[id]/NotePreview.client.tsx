67a18584267668dbbc79cee5ffcf447440df7482
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2025-12-28 21:22:36 +0100

    add serverApi to page.tsx and add clientApi to  NotePreview.client.tsx

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -4,7 +4,7 @@ import Modal from '@/components/Modal/Modal';
 import { useRouter } from 'next/navigation';
 import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
-import { fetchNoteById } from '@/lib/api';
+import { fetchNoteById } from '@/lib/api/clientApi';
 import { Note } from '@/types/note';
 
 export default function NotePreviewClient() {
--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { fetchNoteById } from '@/lib/api';
+import { fetchNoteByIdServer } from '@/lib/api/serverApi';
 import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import NotePreviewClient from './NotePreview.client';
 
@@ -6,13 +6,13 @@ interface Props {
   params: Promise<{ id: string }>;
 }
 
-export default async function NoteDetails({ params }: Props) {
+export default async function NotePreview({ params }: Props) {
   const queryClient = new QueryClient();
   const { id } = await params;
 
   await queryClient.prefetchQuery({
     queryKey: ['note', id],
-    queryFn: () => fetchNoteById(id),
+    queryFn: () => fetchNoteByIdServer(id),
   });
 
   return (
